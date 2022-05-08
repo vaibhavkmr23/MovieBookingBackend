7aa794e69d048d62b9caee3e42ccd584916f96c1
vaibhav <vaibhavkumarjha46@gmail.com>
2022-05-09 02:34:44 +0530

    Checkpoint 3 complete

---
+++ server.js
@@ -0,0 +1,26 @@
+const port = 9000;
+const http = require("http");
+const httpStatus = require("http-status-codes");
+
+const routeResponseMap ={
+    "/movies": "All Movies Data in JSON format from Mongo DB",
+    "/genres": "All Genres Data in JSON format from Mongo DB",
+    "/artists": "All Artists Data in JSON format from Mongo DB"
+};
+
+
+const app = http.createServer((req, res) =>{
+    console.log("Received an incoming request!");
+    if(routeResponseMap[req.url]){
+        res.writeHead(httpStatus.StatusCodes.OK, {"Content-Type": "text/html"});
+        res.end(routeResponseMap[req.url]);
+    }
+    else{
+        res.writeHead(httpStatus.StatusCodes.NOT_FOUND);
+        res.end();
+    }
+
+})
+app.listen(port);
+
+console.log(`The server has started and is listening on port number: ${port}`);
