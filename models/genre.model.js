270d251f609b0c45771b1e94c6a36a0bb7a84a72
vaibhav <vaibhavkumarjha46@gmail.com>
2022-05-12 17:54:02 +0530

    Added edited URL in Db Config File

---
+++ models/genre.model.js
@@ -0,0 +1,12 @@
+// module.exports = (mongoose) => {
+//     const Genre = mongoose.model(
+//         "genre",
+//         mongoose.Schema(
+//             {
+//                 genreid: Number,
+//                 genre: String,
+//             }
+//         )
+//     );
+//     return Genre;
+// };
--- server.js
+++ server.js
@@ -2,20 +2,22 @@ const port = 9000;
 const http = require("http");
 const httpStatus = require("http-status-codes");
 
-const routeResponseMap ={
+// const db = require("./models");
+
+const routeResponseMap = {
     "/movies": "All Movies Data in JSON format from Mongo DB",
     "/genres": "All Genres Data in JSON format from Mongo DB",
     "/artists": "All Artists Data in JSON format from Mongo DB"
 };
 
 
-const app = http.createServer((req, res) =>{
+const app = http.createServer((req, res) => {
     console.log("Received an incoming request!");
-    if(routeResponseMap[req.url]){
-        res.writeHead(httpStatus.StatusCodes.OK, {"Content-Type": "text/html"});
+    if (routeResponseMap[req.url]) {
+        res.writeHead(httpStatus.StatusCodes.OK, { "Content-Type": "text/html" });
         res.end(routeResponseMap[req.url]);
     }
-    else{
+    else {
         res.writeHead(httpStatus.StatusCodes.NOT_FOUND);
         res.end();
     }
@@ -24,3 +26,19 @@ const app = http.createServer((req, res) =>{
 app.listen(port);
 
 console.log(`The server has started and is listening on port number: ${port}`);
+
+////-----------------------------///
+
+// db.mongoose
+//     .connect(db.url, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+//     .then(() => {
+//         console.log("Connected to the database!");
+
+//     })
+//     .catch(err => {
+//         console.log("Cannot connect to the database!", err);
+//         process.exit();
+//     });
